73900f1ed22b3dc0403efd80d3cf8a4f1ce4be25
LiS <world.life.destiny@icloud.com>
2024-01-11 19:54:23 +0300

    rev2

--- src/main/java/org/example/tobot/BotService.java
+++ src/main/java/org/example/tobot/BotService.java
@@ -2,11 +2,16 @@ package org.example.tobot;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.example.tobot.dto.CentralRussianBankService;
+import org.example.tobot.dto.ValuteCursOnDate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import javax.annotation.PostConstruct;
 
@@ -29,7 +34,28 @@ public class BotService extends TelegramLongPollingBot {
     }
     @Override
     public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        try {
+            SendMessage response = new SendMessage();
+            Long chatId = message.getChatId();
+            response.setChatId(String.valueOf(chatId));
 
+
+            if ("/currentrates".equalsIgnoreCase(message.getText())) {
+
+                for (ValuteCursOnDate valuteCursOnDate : centralRussianBankService.getCurrenciesFromCbr()) {
+
+
+                    response.setText(StringUtils.defaultIfBlank(response.getText(), "") + valuteCursOnDate.getName() + " - " + valuteCursOnDate.getCourse() + "\n");
+                }
+            }
+
+            execute(response);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
